cc7f9b8bb5d78a489abde683189a6a9326ea2e85
Sebas2021herrera <sebastianherrera2108@gmail.com>
2021-06-30 20:52:04 -0500

    segundo repos

--- SEMANA 1/ejemplo2/src/App.java
+++ SEMANA 1/ejemplo2/src/App.java
@@ -2,13 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         //length(); charAt(n); substring(n1,n2); toUpperCase(); toLowerCase(); trim()
         String cadena = "El perro (Canis familiaris o Canis lupus familiaris dependiendo de si se lo considera una especie por derecho propio o una subespecie del lobo)";
-        System.out.println(cadena.length()+"\n"+ cadena.charAt(40) + "\n"+cadena.substring(2,3)+"\n"+ cadena.toUpperCase() + "\n"+);
-        System.out.println(cadena.charAt(40));
-        System.out.println(cadena.substring(2,3));
-        System.out.println(cadena.toUpperCase());
-        System.out.println(cadena.toLowerCase());
-        System.out.println(cadena.trim());
-            
+        System.out.println(cadena.length()+"\n"+ cadena.charAt(40) + "\n"+cadena.substring(2,3)+"\n"+ cadena.toUpperCase() + "\n"+cadena.toLowerCase()+"\n"+cadena.trim());
 
     }
 }
